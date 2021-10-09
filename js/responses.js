dc49536e8be25d51928cb5c3e0fc9cdc2d8defd6
Md. Mannan Sidker <68600457+mannan-sikder@users.noreply.github.com>
2021-10-09 19:15:04 +0600

    this is chatbot website
    this is demo chatbot site

---
+++ js/responses.js
@@ -0,0 +1,22 @@
+function getBotResponse(input) {
+    //rock paper scissors
+    if (input == "rock") {
+        return "paper";
+    } else if (input == "paper") {
+        return "scissors";
+    } else if (input == "scissors") {
+        return "rock";
+    }
+    else if (input ="price" ){
+        return "To know please call at +8801922421768";
+    }
+
+    // Simple responses
+    if (input == "hello") {
+        return "Hello there!";
+    } else if (input == "goodbye") {
+        return "Talk to you later!";
+    } else {
+        return "Try asking something else!";
+    }
+}
